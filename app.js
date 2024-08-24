ca992fea301f1ef33e1c8f89b4d832b0b204a4d7
Yasshhh12 <prasyast152@gmail.com>
2024-08-25 03:05:36 +0530

    Established mongoose connection

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+// app.use('/api/emails', emailRoutes);
+
+mongoose.connect('mongodb://127.0.0.1:27017/emailservice').then(e=>console.log('MongoDB connected'));
+
+module.exports = app;
